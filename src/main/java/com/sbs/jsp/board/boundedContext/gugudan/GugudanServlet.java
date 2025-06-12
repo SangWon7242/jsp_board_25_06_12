b7cfe549bd3c12fd9236cbe3885901721d48c402
SangWon7242 <gotkdqja@gmail.com>
2025-06-12 21:48:20 +0900

    모델 2 방식 적용

--- src/main/java/com/sbs/jsp/board/boundedContext/gugudan/GugudanServlet.java
+++ src/main/java/com/sbs/jsp/board/boundedContext/gugudan/GugudanServlet.java
@@ -2,6 +2,7 @@ package com.sbs.jsp.board.boundedContext.gugudan;
 
 import com.sbs.jsp.board.boundedContext.global.base.Rq;
 import com.sbs.jsp.board.boundedContext.home.HomeServlet;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,18 +18,13 @@ public class GugudanServlet extends HomeServlet {
 
     int dan = rq.getIntParam("dan", 9);
     int limit = rq.getIntParam("limit", 9);
-    rq.appendBody("<h1>== %d단 ==</h1>\n".formatted(dan));
 
-    for(int i = 1; i <= limit; i++) {
-      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-    }
-
-    rq.appendBody("""
-        <div class="box-1"></div>
-        
-        <style>
-          .box-1 { width: 200px; height: 200px; background-color: red; }
-        </style>
-        """);
+    // setAttribute : key, value 형태로 인자값을 넘김
+    req.setAttribute("dan", dan);
+    req.setAttribute("limit", limit);
+    
+    // JSP한테 요청을 보냄
+    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/jsp/gugudan2.jsp");
+    requestDispatcher.forward(req, resp);
   }
 }
