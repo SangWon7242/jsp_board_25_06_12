a030aaa9247f19f975e54acd517678fa08e1de2c
SangWon7242 <gotkdqja@gmail.com>
2025-06-17 19:43:11 +0900

    게시물 역순 출력

--- src/main/java/com/sbs/jsp/board/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/sbs/jsp/board/boundedContext/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.sbs.jsp.board.boundedContext.article.dto.Article;
 import com.sbs.jsp.board.boundedContext.global.base.Rq;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -24,6 +25,8 @@ public class ArticleController {
   public void showList(Rq rq) {
     // 원본을 기반으로 복사본을 만들어 연결
     List<Article> articles = new ArrayList<>(articleList);
+
+    Collections.reverse(articles);
 
     rq.setAttr("articles", articles);
 
